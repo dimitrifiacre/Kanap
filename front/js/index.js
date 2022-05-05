a03f538b548c2092624f8767a65d0d91815c6137
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-05-05 14:22:52 +0200

    Display data by ID on product page

--- front/js/index.js
+++ front/js/index.js
@@ -1,41 +1,41 @@
 // On récupère les produits de l'API
 function getProduct() {
-    fetch("http://localhost:3000/api/products")
-        .then((response) => {
-            return response.json();
-        })
-        .catch((error) => {
-            let sectionItems = document.querySelector("#items");
-            sectionItems.innerHTML = "Aucun produit n'a été trouvé";
-        })
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => {
+      let sectionItems = document.querySelector("#items");
+      sectionItems.innerHTML = "Aucun produit n'a été trouvé";
+    })
 
-        // On affiche les données de l'API dans le DOM
-        .then((resultAPI) => {
-            const products = resultAPI;
-            for (let product in products) {
-                let productCard = document.createElement("a");
-                document.querySelector("#items").appendChild(productCard);
-                productCard.href = `./product.html?id=${resultAPI[product]._id}`;
+    // On affiche les données de l'API dans le DOM
+    .then((resultAPI) => {
+      const products = resultAPI;
+      for (let product in products) {
+        let productCard = document.createElement("a");
+        document.querySelector("#items").appendChild(productCard);
+        productCard.href = `./product.html?id=${resultAPI[product]._id}`;
 
-                let productArticle = document.createElement("article");
-                productCard.appendChild(productArticle);
+        let productArticle = document.createElement("article");
+        productCard.appendChild(productArticle);
 
-                let productImage = document.createElement("img");
-                productArticle.appendChild(productImage);
-                productImage.src = resultAPI[product].imageUrl;
-                productImage.alt = resultAPI[product].altTxt;
+        let productImage = document.createElement("img");
+        productArticle.appendChild(productImage);
+        productImage.src = resultAPI[product].imageUrl;
+        productImage.alt = resultAPI[product].altTxt;
 
-                let productName = document.createElement("h3");
-                productArticle.appendChild(productName);
-                productName.classList.add("productName");
-                productName.innerHTML = resultAPI[product].name;
+        let productName = document.createElement("h3");
+        productArticle.appendChild(productName);
+        productName.classList.add("productName");
+        productName.innerHTML = resultAPI[product].name;
 
-                let productDescription = document.createElement("p");
-                productArticle.appendChild(productDescription);
-                productDescription.classList.add("productDescription");
-                productDescription.innerHTML = resultAPI[product].description;
-            }
-        })
+        let productDescription = document.createElement("p");
+        productArticle.appendChild(productDescription);
+        productDescription.classList.add("productDescription");
+        productDescription.innerHTML = resultAPI[product].description;
+      }
+    });
 }
 // On lance la fonction (provisoirement)
 getProduct();
---
+++ front/js/product.js
@@ -0,0 +1,56 @@
+// On cherche les paramètres de l'URL
+let searchParams = new URLSearchParams(window.location.search);
+let searchID = searchParams.get("id");
+
+// On vérifie si l'URL a bien un ID
+function checkProduct() {
+  if (searchParams.has('id')) {
+    let productID = searchParams.get('id');
+  } else {
+    let sectionItem = document.querySelector(".item");
+    sectionItem.innerHTML = "Le produit n'a pas été trouvé";
+    document.title = `Produit indisponible – Kanap`;
+  }
+}
+
+// On récupère les données du produit en fonction de son ID
+function showProduct() {
+  fetch(`http://localhost:3000/api/products/${searchID}`)
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => {
+      let sectionItems = document.querySelector(".item");
+      sectionItems.innerHTML = "Le produit n'a pas été trouvé";
+    })
+
+    // On affiche les données de l'API dans le DOM
+    .then((resultAPI) => {
+      document.title = `Canapé "${resultAPI.name}" – Kanap`;
+
+      let productImage = document.createElement("img");
+      document.querySelector(".item__img").appendChild(productImage);
+      productImage.src = resultAPI.imageUrl;
+      productImage.alt = resultAPI.altTxt;
+
+      let productTitle = document.querySelector("#title");
+      productTitle.innerHTML = resultAPI.name;
+
+      let productPrice = document.querySelector("#price");
+      productPrice.innerHTML = resultAPI.price;
+
+      let productDescription = document.querySelector("#description");
+      productDescription.innerHTML = resultAPI.description;
+
+      let productColors = document.querySelector("#colors");
+      for (let i = 0; i < resultAPI.colors.length; i++) {
+        let productColorsOption = document.createElement("option");
+        productColorsOption.innerHTML = resultAPI.colors[i];
+        productColors.appendChild(productColorsOption);
+      }
+    });
+}
+
+// On lance les fonctions (provisoirement)
+checkProduct();
+showProduct();
